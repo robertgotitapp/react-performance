9a03ecfe00bc11240b4450596476cd49cd6a1782
Robert Nguyen <robert@gotitapp.co>
2021-04-19 22:09:24 +0700

    fix: ex 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,20 +1,14 @@
 // Code splitting
 // http://localhost:3000/isolated/exercise/01.js
-
+// import Globe from '../globe'
+// import(/* webpackPrefetch: true */ '../globe')
 import * as React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
 
-// 🐨 use React.lazy to create a Globe component which uses a dynamic import
-// to get the Globe component from the '../globe' module.
+const Globe = React.lazy(() => import(/* webpackPrefetch: true */'../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
 
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
   return (
     <div
       style={{
@@ -26,7 +20,9 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      <label 
+        style={{marginBottom: '1rem'}}
+      >
         <input
           type="checkbox"
           checked={showGlobe}
@@ -34,9 +30,11 @@ function App() {
         />
         {' show globe'}
       </label>
-      <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
-      </div>
+        <div style={{width: 400, height: 400}}>
+          <React.Suspense fallback={<div>loading...</div>}>
+            {showGlobe ? <Globe /> : null}
+          </React.Suspense>
+        </div>
     </div>
   )
 }
