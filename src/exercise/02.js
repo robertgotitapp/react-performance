c7a6dccd7024f590192df48b6d22d71f985fb063
Robert Nguyen <robert@gotitapp.co>
2021-04-19 23:00:33 +0700

    feat: ex 2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,8 +3,8 @@
 
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
-import {getItems} from '../filter-cities'
-import {useForceRerender} from '../utils'
+import { getItems } from '../workerized-filter-cities'
+import {useForceRerender, useAsync} from '../utils'
 
 function Menu({
   items,
@@ -59,9 +59,11 @@ function ListItem({
 function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
+  const {run, data: allItems } = useAsync({data : [], status: 'pending'}) 
 
-  // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = getItems(inputValue)
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [run, inputValue])
   const items = allItems.slice(0, 100)
 
   const {
